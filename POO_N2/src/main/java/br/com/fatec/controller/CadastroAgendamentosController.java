8830c51babeaac2e3fbd09c5729e7f7c7edbdb74
Vitor Mimaki <usuario@DESKTOP-CA84T61>
2024-11-21 18:46:57 -0300

    Exemplos de DAO e de ComboBox, FINALIZAR!

--- POO_N2/src/main/java/br/com/fatec/controller/CadastroAgendamentosController.java
+++ POO_N2/src/main/java/br/com/fatec/controller/CadastroAgendamentosController.java
@@ -4,9 +4,20 @@
  */
 package br.com.fatec.controller;
 
+import br.com.fatec.dao.AgendamentosDAO;
+import br.com.fatec.model.Agendamentos;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -20,7 +31,92 @@ public class CadastroAgendamentosController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        //faz a ligação da coluna do tableView com 
+        //os atributos da classe Proprietario
+        colSelecionado.setCellValueFactory(
+                new PropertyValueFactory<>("selecionado"));
+        colNome.setCellValueFactory(
+                new PropertyValueFactory<>("nome"));
+        colCodigo.setCellValueFactory(
+                new PropertyValueFactory<>("codProprietario"));
+        
+        //coloca o checkbox na coluna
+        colSelecionado.setCellFactory(
+                CheckBoxTableCell.forTableColumn(colSelecionado));
+
+        //preenche a tabela
+        tblProprietario.setItems(preencheTabela());
     }    
     
+    private ObservableList<Agendamentos> preencheTabela() {
+        AgendamentosDAO dao = new AgendamentosDAO();
+        ObservableList<Agendamentos> agendamentos
+            = FXCollections.observableArrayList();
+        
+        try {
+            //busca somente que termina com 'a'
+            //proprietarios.addAll(dao.lista("nome like '%a'"));
+            //busca todo mundo
+            agendamentos.addAll(dao.lista(""));
+        } catch (SQLException ex) {
+            Alert alerta = new Alert(Alert.AlertType.ERROR,
+                    "Erro Preenche Tabela: " + ex.getMessage(),
+                    ButtonType.OK);
+            alerta.showAndWait();
+        }
+        
+        return agendamentos;
+    }
+    
+    //------------------------- COMBO BOX -----------------------------
+    
+    private void configuraChangeValueComboProduto() {
+        //programando o evento change da combo para
+        //exibir seu conteudo nos texts
+        cbProduto.valueProperty().addListener((value, velho, novo) -> {
+            if(novo != null) {
+                txtCodigo.setText(Integer.toString(novo.getCodigo()));
+                txtDescricao.setText(novo.getDescricao());
+                txtPreco.setText(String.valueOf(novo.getPreco()));
+            }
+        });
+    }
+        
+    @FXML
+    private void btnPreencher_Click(ActionEvent event) {
+        listaProdutos.clear();
+        txtCodigo.clear();
+        txtDescricao.clear();
+        txtPreco.clear();
+                
+        listaProdutos.add(new Produto(1, "Caneta", 5.87f));
+        listaProdutos.add(new Produto(2, "Regua", 10.20f));
+        listaProdutos.add(new Produto(3, "Compasso", 15.80f));
+    }
+
+    @FXML
+    private void btnExibir_Click(ActionEvent event) {
+        Alert alerta = new Alert(Alert.AlertType.ERROR);
+        alerta.setTitle("Mensagem");
+        alerta.setContentText("");
+
+        if(cbProduto.getValue() == null) {
+            alerta.setHeaderText("Tem que selecionar um item");
+            alerta.showAndWait();
+        }
+        else {
+            //obtem o objeto que está selecionado na combo
+            Produto p = cbProduto.getValue();
+
+            //monta o que será exibido
+            StringBuilder texto = new StringBuilder();
+            texto.append("Produto Selecionado: ").append(p.getDescricao());
+            texto.append("\nCodigo: ").append(String.valueOf(p.getCodigo()));
+            texto.append("\nPreço: ").append(String.valueOf(p.getPreco()));
+            
+            alerta.setHeaderText(texto.toString());
+
+            alerta.showAndWait(); //exibe a mensage
+        }
+    }
 }
--- POO_N2/src/main/java/br/com/fatec/dao/AgendamentosDAO.java
+++ POO_N2/src/main/java/br/com/fatec/dao/AgendamentosDAO.java
@@ -5,7 +5,9 @@
 package br.com.fatec.dao;
 
 import br.com.fatec.model.Agendamentos;
+import br.com.fatec.persistencia.Banco;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -13,30 +15,175 @@ import java.util.Collection;
  * @author usuario
  */
 public class AgendamentosDAO implements DAO<Agendamentos> {
-
+    
+    //variaveis auxiliares
+    //permite o uso de comandos DML (select, insert, delete e update) para
+    //acessar nosso SGBD
+    private java.sql.PreparedStatement pst;
+    
+    //permite armazenar um conjunto de dados vindo do SGBD para ser
+    //manipulado
+    private java.sql.ResultSet rs;
+    
+    //representar os dados do  meu negócio
+    private Agendamentos agendamento; //meu MODEL   
+    
     @Override
     public boolean insere(Agendamentos obj) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        //String sql = "INSERT INTO Agendamentos (codProprietario, nome) " +
+        //        " VALUES (?, ?)"; //a ? indica parametros
+        
+        //abre a conexao com o banco
+        Banco.conectar();
+        //preparar o comando PST
+        pst = Banco.obterConexao().prepareStatement(sql);
+        
+        //associar os dados do objeto Proprietario com o comando INSERT
+        //pst.setInt(1, obj.getCodProprietario());
+        //pst.setString(2, obj.getNome());
+        
+        //executar o comando
+        int res = pst.executeUpdate(); //esse método serve para Insert, delete e update
+        
+        //fecha a conexao
+        Banco.desconectar();
+        
+        //devolve se funcionou ou nao
+        return res != 0;
+        
     }
 
     @Override
     public boolean remove(Agendamentos obj) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        //String sql = "DELETE FROM Agendamentos WHERE codProprietario = ?"; //a ? indica parametros
+        
+        //abre a conexao com o banco
+        Banco.conectar();
+        //preparar o comando PST
+        pst = Banco.obterConexao().prepareStatement(sql);
+        
+        //associar os dados do objeto Proprietario com o comando DELETE
+        //pst.setInt(1, obj.getCodProprietario());
+        
+        //executar o comando
+        int res = pst.executeUpdate(); //esse método serve para Insert, delete e update
+        
+        //fecha a conexao
+        Banco.desconectar();
+        
+        //devolve se funcionoou ou nao
+        return res != 0;
     }
 
     @Override
     public boolean altera(Agendamentos obj) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        //String sql = "UPDATE Agendamentos SET Nome = ? "
+        //        + "WHERE codProprietario = ?"; //a ? indica parametros
+        
+        //abre a conexao com o banco
+        Banco.conectar();
+        //preparar o comando PST
+        pst = Banco.obterConexao().prepareStatement(sql);
+        
+        //associar os dados do objeto Proprietario com o comando UPDATE
+        //pst.setInt(2, obj.getCodProprietario());
+        //pst.setString(1, obj.getNome());
+        
+        //executar o comando
+        int res = pst.executeUpdate(); //esse método serve para Insert, delete e update
+        
+        //fecha a conexao
+        Banco.desconectar();
+        
+        //devolve se funcionoou ou nao
+        return res != 0;
     }
 
+    /**
+     * Busca um dado de proprietario baseado em sua Chave Primaria
+     * @param obj
+     * @return
+     * @throws SQLException 
+     */
     @Override
     public Agendamentos buscaID(Agendamentos obj) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        //String sql = "SELECT * FROM Agendamentos "
+        //        + "WHERE codProprietario = ?"; //a ? indica parametros
+        
+        //abre a conexao com o banco
+        Banco.conectar();
+        //preparar o comando PST
+        pst = Banco.obterConexao().prepareStatement(sql);
+        
+        //associar os dados do objeto Proprietario com o comando UPDATE
+        //pst.setInt(1, obj.getCodProprietario());
+        
+        //executar o comando
+        rs = pst.executeQuery(); //esse método serve para SELECT
+        
+        //verificar se trouxe algum registro
+        //rs.next() faz a leitura do próximo registro, se existir devolve true
+        //se nao devolve false
+        if(rs.next()) {
+            //mover os dados(campos da tab) do resultSet para o objeto proprietário
+            agendamentos = new Agendamentos();
+            agendamentos.setCodProprietario(rs.getInt("codProprietario"));
+            agendamentos.setNome(rs.getString("Nome"));
+        }
+        else {
+            //não encontrou o registro solicitado
+            agendamentos = null;
+        }
+                
+        //fecha a conexao
+        Banco.desconectar();
+        
+        //devolve o objeto proprietario
+        return agendamentos;
+
     }
 
     @Override
     public Collection<Agendamentos> lista(String criterio) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        //cria uma lista para armazenar os dados vindos do banco
+        ArrayList<Agendamentos> lista = new ArrayList<>();
+        
+        String sql = "SELECT * FROM Agendamentos ";
+
+        //precisa fazer filtro para listagem
+        if(criterio != null && criterio.length() > 0) {
+            sql += " WHERE " + criterio;
+        }
+        
+        //abre a conexao com o banco
+        Banco.conectar();
+        
+        //preparar o comando PST
+        pst = Banco.obterConexao().prepareStatement(sql);
+        
+        //executar o comando
+        rs = pst.executeQuery(); //esse método serve para SELECT
+        
+        //Varre todo o resultado da consulta e coloca cada registro dentro
+        //de um objeto e coloca o objeto dentro da coleção
+        while(rs.next()) {
+            //criar o objeto
+            agendamentos = new Agendamentos();
+            
+            //mover os dados do resultSet para o objeto proprietário
+            agendamentos.setCodProprietario(rs.getInt("codProprietario"));
+            agendamentos.setNome(rs.getString("Nome"));
+            
+            //move o objeto para a coleção
+            lista.add(agendamentos);
+        }
+                
+        //fecha a conexao
+        Banco.desconectar();
+        
+        //devolve o objeto proprietario
+        return lista;
+        
     }
     
 }
